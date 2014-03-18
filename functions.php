689c57271512f023d68d92b39d50672af3e041fe
Hugo Bessa <hugo.bessa@mktvirtual.com.br>
2014-03-18 14:11:02 -0300

    Required plugins prompt removed

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php
-require_once locate_template('/libs/required-plugins.php');
 require_once locate_template('/libs/init.php');
 require_once locate_template('/libs/clear-defaults.php');
 require_once locate_template('/libs/utils.php');
