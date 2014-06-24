6b49b5739fce83b451a8b576dd0ac04520e18690
Hugo Bessa <hugo.bessa@mktvirtual.com.br>
2014-06-24 12:00:27 -0300

    Update Gruntfile
    For this new theme I want to make things easier and prettier. This
    led me to update the way Grunt works with the Theme.
    
    Right now the Gruntfile is pretty simple and just do js hintin, sass
    compilation and image optimization. But this is a new structure to build
    against.
    
    I also choose to go with .js instead of .coffee, as not everybody is using
    .coffee here in Mkt Virtual.

---
+++ Gruntfile.js
@@ -0,0 +1,135 @@
+var _ = require('lodash');
+
+module.exports = function(grunt) {
+    'use-strict';
+
+    /* Initialize configuration
+    -----------------------------------------------------*/
+
+    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
+
+    // default config
+    var config = {
+        compass: false
+    };
+
+    // apply command line configuration
+    _.forEach(grunt.option.flags(), function(option) {
+        config[option] = grunt.option(option);
+    });
+
+    // default paths
+    var paths = {
+        assets: 'assets',
+        build:  'assets',
+        sass:   'sass',
+        scss:   'scss',
+        css:    'css',
+        js:     'js',
+        img:    'img'
+    };
+
+    /* Tasks
+    -----------------------------------------------------*/
+
+    var tasks = {};
+    tasks.paths = paths;
+
+    // Watch---------------------------
+    // [TODO]: add support to compass
+    // [TODO]: add support to coffee
+    tasks.watch = {
+        gruntfile: {
+            files: ['./Gruntfile.js'],
+            options: {
+                reload: true
+            }
+        },
+
+        stylesheets: {
+            files: ['<%= paths.assets %>/<%= paths.scss %>/**/*.scss'],
+            tasks: ['sass']
+        }
+    };
+
+    // BrowserSync --------------------
+    // [TODO]: configure proxy option
+    tasks.browserSync = {
+        dev: {
+            bsFiles: {
+                src: [
+                    '<%= paths.build %>/<%= paths.css %>/**/*.css',
+                    '<%= paths.build %>/<%= paths.img %>/**/*.{png,jpg,gif}',
+                    '<%= paths.build %>/<%= paths.js %>/**/*.js',
+                    '**/*.php'
+                ]
+            },
+            options: {
+                watchTask: true,
+                ghostMode: {
+                    location: true
+                }
+            }
+        }
+    };
+
+    // Sass ---------------------------
+    tasks.sass = {
+        dev: {
+            files: [{
+                expand: true,
+                cwd: '<%= paths.assets %>/<%= paths.scss %>/',
+                src: [
+                    '**/*.scss'
+                ],
+                dest: '<%= paths.build %>/<%= paths.css %>',
+                ext: '.css',
+                extDot: 'last'
+            }],
+            options: {
+                style: 'compressed',
+                sourcemap: true,
+                compass: config.compass
+            }
+        }
+    };
+
+    // Imagemin -----------------------
+    tasks.imagemin = {
+        dev: {
+            files: [{
+                expand: true,
+                cwd: '<%= paths.assets %>/<%= paths.img %>',
+                src: '**/*.{png,jpg,gif,svg}',
+                dest: '<%= paths.build %>/<%= paths.img %>'
+            }],
+            options: {
+                optimizationLevel: 7,
+                use: [
+                    'imagemin-optipng',
+                    'imagemin-jpegtran',
+                    'imagemin-gifsicle'
+                ]
+            }
+        }
+    };
+
+    // configures grunt
+    grunt.initConfig(tasks);
+
+
+    /* Group Tasks
+    -----------------------------------------------------*/
+
+    grunt.registerTask('build', [
+        'sass',
+        'imagemin'
+    ]);
+
+    grunt.registerTask('default', [
+        'browserSync',
+        'watch'
+    ]);
+
+};
