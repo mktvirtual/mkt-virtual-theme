4098664bb75e145094066be245e72b70fd8350f7
Hugo Bessa <hugo.bessa@mktvirtual.com.br>
2014-06-24 12:31:28 -0300

    Readd populate_array function in utils.php

--- lib/utils.php
+++ lib/utils.php
@@ -9,3 +9,18 @@ function debug($value=''){
 	  var_dump($value);
 	  echo '</pre>';
 }
+
+function populate_array(&$target, $values, $begining=false){
+  if ($begining) {
+    if (isset($target)) {
+      foreach ($target as $tgt) {
+        $values[] = $tgt;
+      }
+    }
+    $target = $values;
+  } else {
+    foreach ($values as $value) {
+      $target[] = $value;
+    }
+  }
+}
