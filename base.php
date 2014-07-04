678a8e91906a9c6dbe0da8e232a590893ab5e4ad
Hugo Bessa <hugo.bessa@mktvirtual.com.br>
2014-07-04 11:11:18 -0300

    Add templates/head
    First step trough responsability decoupling

--- base.php
+++ base.php
@@ -3,8 +3,11 @@
  * Base Wrapper
  */
 
+get_template_part('templates/head');
+
 get_header();
 
     include mkt_template_path();
 
 get_footer();
+?>
--- header.php
+++ header.php
@@ -1,32 +1,3 @@
-<!DOCTYPE HTML>
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="pt-BR"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="pt-BR"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" lang="pt-BR"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="pt-BR"> <!--<![endif]-->
-<head>
-  <meta charset="utf-8">
-  <title><?php wp_title(); ?></title>
-  <link rel="icon" href="<?php echo PATH_IMAGES.'/favicon.png' ?>">
-  <link rel="alternate" type="application/+xml" title="<?php bloginfo('name'); ?> Feed" href="<?php site_url(); ?>/feed/">
-  <meta name="viewport" content="width=1024">
-  <meta name="author" content="">
-
-  <?php
-
-  // Default css
-  global $css;
-
-  $default_stylesheets = array(
-    'css/page.defaults.css'
-  );
-  populate_array($css, $default_stylesheets);
-
-  get_stylesheets();
-  wp_head();
-  ?>
-
-  <script src="<?php echo PATH_TEMPLATE.'/assets/bower_components/modernizr-shim/modernizr.min.js' ?>"></script>
-</head>
 <body <?php body_class() ?>>
   <div class="root" id="root">
     <?php
