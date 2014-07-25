ef6596ad5ec0098878c877a403329cc4b18f1766
Hugo Bessa <hugo.bessa@mktvirtual.com.br>
2014-07-25 17:59:56 -0300

    Fix indentation

--- lib/utils.php
+++ lib/utils.php
@@ -1,26 +1,26 @@
 <?php
 function debug_query(){
-	  global $wp_query;
-	  debug($wp_query);
+    global $wp_query;
+    debug($wp_query);
 }
 
 function debug($value=''){
-	  echo '<pre class="debug">';
-	  var_dump($value);
-	  echo '</pre>';
+    echo '<pre class="debug">';
+    var_dump($value);
+    echo '</pre>';
 }
 
 function populate_array(&$target, $values, $begining=false){
-  if ($begining) {
-    if (isset($target)) {
-      foreach ($target as $tgt) {
-        $values[] = $tgt;
-      }
+    if ($begining) {
+        if (isset($target)) {
+            foreach ($target as $tgt) {
+                $values[] = $tgt;
+            }
+        }
+        $target = $values;
+    } else {
+        foreach ($values as $value) {
+            $target[] = $value;
+        }
     }
-    $target = $values;
-  } else {
-    foreach ($values as $value) {
-      $target[] = $value;
-    }
-  }
 }
