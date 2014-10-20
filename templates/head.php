c1ec732bedff74acf6e6752a7dfb1ff5d000a8ee
Hugo Bessa <hugo.bessa@mktvirtual.com.br>
2014-10-20 17:18:27 -0200

    Add mobile icons

--- templates/head.php
+++ templates/head.php
@@ -6,8 +6,17 @@
     <meta name="author" content="Mkt Virtual">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title(); ?></title>
+    <meta name="application-name" content="<?php echo bloginfo('name'); ?>" />
 
+    <!-- Favicon -->
     <link rel="icon" href="<?php echo PATH_IMAGES.'/favicon.png' ?>">
+    <!-- iOS icon -->
+    <link rel="apple-touch-icon" href="<?php echo PATH_IMAGES.'/favicon-ios-precomposed.png' ?>">
+    <!-- Windows Phone icon -->
+    <meta name="msapplication-TileImage" content="<?php echo PATH_IMAGES.'/favicon-wp.png' ?>">
+    <meta name="msapplication-TileColor" content="#2a9f90"/>
+
+    <!-- RSS Feed -->
     <link rel="alternate" type="application/+xml" href="<?php bloginfo('rss2_url'); ?>">
 
     <?php
