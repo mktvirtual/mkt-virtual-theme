36e7da975f94f90014d2f531f3d9928564c9d7dd
Hugo Bessa <hugo.bessa@mktvirtual.com.br>
2014-11-12 11:13:07 -0200

    Fix browser-sync
    The client script has moved to be in a browser-sync folder

--- templates/browser-sync.php
+++ templates/browser-sync.php
@@ -4,6 +4,6 @@
  */
 if($_SERVER['SERVER_PORT'] == '8888'){
     echo '<script type=\'text/javascript\'>//<![CDATA[
-;document.write("<script async src=\'//HOST:3000/browser-sync-client.js\'><\/script>".replace(/HOST/g, location.hostname));
+;document.write("<script async src=\'//HOST:3000/browser-sync/browser-sync-client.js\'><\/script>".replace(/HOST/g, location.hostname));
 //]]></script>';
 }
